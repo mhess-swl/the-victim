23456131d6fea6b1c5c8060990619a4b3e0fdafd
Kelly Greco <kelly@swirldslabs.com>
2025-05-07 07:57:11 -0400

    update app greeting

--- app/src/main/java/org/example/App1.java
+++ app/src/main/java/org/example/App1.java
@@ -5,7 +5,7 @@ package org.example;
 
 public class App1 {
     public String getGreeting() {
-        return "mhess-swl thinks he owns App1";
+        return "mhess-swl thinks he owns App1, but he doesn't";
     }
 
     public static void main(String[] args) {
